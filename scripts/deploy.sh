a39c58f8fcc020404baef3fe7a51956182f1efba
Yoshino-s <yoshino.prog@gmail.com>
2020-02-19 14:51:35 +0800

    add gh-pages

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,10 +4,15 @@ set -e
 
 cd dist/pwa
 git init
-git checkout -b coding-pages
+git remote add origin git@github.com:Yoshino-s/schedule.git
+git remote add coding-origin git@e.coding.net:Yoshino-s/sche.git
 git add .
 git commit -m "deploy"
-git remote add origin git@e.coding.net:Yoshino-s/sche.git
-git push --force origin coding-pages
+
+git checkout -b gh-pages
+git push --force origin gh-pages
+
+git checkout -b coding-pages
+git push --force coding-origin coding-pages
 
 echo "deploy success."
